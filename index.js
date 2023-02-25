9edcd503e5786f40c01fe099265a29a6a0897bdf
Thomas Cazade <cazade.thomas@gmail.com>
2023-02-25 08:10:21 +0100

    feat: initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  extends: [
+    "next/core-web-vitals",
+    "airbnb",
+    "airbnb-typescript",
+    "plugin:prettier/recommended"
+  ],
+
+  rules: {
+    "import/prefer-default-export": "off",
+    "react/jsx-props-no-spreading": "off",
+    "react/no-array-index-key": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/require-default-props": "off"
+  }
+};
