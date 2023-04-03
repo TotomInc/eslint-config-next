997fafc4043c06cc55b060c82358d89c5b7840f8
Thomas Cazade <cazade.thomas@gmail.com>
2023-04-03 14:13:47 +0200

    feat: add plugin-comments, plugin-jsonc and plugin-markdown

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: [
+  "extends": [
     "next/core-web-vitals",
     "airbnb",
     "airbnb-typescript",
-    "plugin:prettier/recommended"
+    "plugin:prettier/recommended",
+
+    "plugin:eslint-comments/recommended",
+    "plugin:jsonc/recommended-with-jsonc",
+    "plugin:jsonc/prettier",
+    "plugin:markdown/recommended",
   ],
 
-  rules: {
+  "rules": {
     "import/prefer-default-export": "off",
     "react/jsx-props-no-spreading": "off",
     "react/no-array-index-key": "off",
@@ -20,9 +25,34 @@ module.exports = {
     {
       "files": ["./pages/api/**/*.ts"],
       "rules": {
-        // Allow console.log in serverless functions endpoints.
+        // Allow `console.log` in serverless functions endpoints.
         "no-console": "off"
       }
-    }
+    },
+    {
+      "files": ["*.json"],
+      "parser": "jsonc-eslint-parser",
+    },
+    {
+      // Code blocks inside Markdown files.
+      "files": ["**/*.md/*.*"],
+      "rules": {
+        "@typescript-eslint/no-redeclare": "off",
+        "@typescript-eslint/no-unused-vars": "off",
+        "@typescript-eslint/no-use-before-define": "off",
+        "@typescript-eslint/no-var-requires": "off",
+        "@typescript-eslint/comma-dangle": "off",
+        "@typescript-eslint/consistent-type-imports": "off",
+        "import/no-unresolved": "off",
+        "unused-imports/no-unused-imports": "off",
+        "unused-imports/no-unused-vars": "off",
+        "no-alert": "off",
+        "no-console": "off",
+        "no-restricted-imports": "off",
+        "no-undef": "off",
+        "no-unused-expressions": "off",
+        "no-unused-vars": "off",
+      },
+    },
   ]
 };
