ee3e829d6881215b8739850c1ac1ea506fb7b181
Thomas Cazade <cazade.thomas@gmail.com>
2024-06-03 10:10:01 +0200

    feat: allow custom user configs as parameter

--- index.ts
+++ index.ts
@@ -1,10 +1,15 @@
 import { FlatCompat } from "@eslint/eslintrc";
 import prettier from "eslint-plugin-prettier";
-import antfu, { GLOB_JSX, GLOB_SRC, GLOB_TSX } from "@antfu/eslint-config";
+import antfu, {
+  GLOB_JSX,
+  GLOB_SRC,
+  GLOB_TSX,
+  type TypedFlatConfigItem,
+} from "@antfu/eslint-config";
 
 const compat = new FlatCompat();
 
-export function totominc() {
+export function totominc(...userConfigs: TypedFlatConfigItem[]) {
   return antfu(
     {
       react: true,
@@ -31,15 +36,21 @@ export function totominc() {
       files: [GLOB_TSX, GLOB_JSX],
       rules: {
         // Extra styling rules not interacting with prettier.
-        "style/jsx-self-closing-comp": ["error", { component: true, html: true }],
-        "style/jsx-sort-props": ["error", {
-          ignoreCase: false,
-          callbacksLast: true,
-          shorthandFirst: true,
-          multiline: "last",
-          noSortAlphabetically: true,
-          reservedFirst: true,
-        }],
+        "style/jsx-self-closing-comp": [
+          "error",
+          { component: true, html: true },
+        ],
+        "style/jsx-sort-props": [
+          "error",
+          {
+            ignoreCase: false,
+            callbacksLast: true,
+            shorthandFirst: true,
+            multiline: "last",
+            noSortAlphabetically: true,
+            reservedFirst: true,
+          },
+        ],
 
         "react-hooks-extra/ensure-custom-hooks-using-other-hooks": "error",
         "react-hooks-extra/ensure-use-callback-has-non-empty-deps": "error",
@@ -51,7 +62,10 @@ export function totominc() {
         // Allow floating promise when `onClick={doSomethingAsync}` with an async function
         // passed to an event handler.
         // See: https://github.com/typescript-eslint/typescript-eslint/issues/4619
-        "@typescript-eslint/no-misused-promises": ["error", { checksVoidReturn: false }],
+        "@typescript-eslint/no-misused-promises": [
+          "error",
+          { checksVoidReturn: false },
+        ],
 
         // Allow using `process.env` without `require("process")`.
         "node/prefer-global/process": "off",
@@ -93,19 +107,29 @@ export function totominc() {
         "style/jsx-one-expression-per-line": ["off"],
 
         "perfectionist/sort-exports": "error",
-        "perfectionist/sort-imports": ["error", {
-          "type": "line-length",
-          "newlines-between": "always",
-          "internal-pattern": ["@/**", "~/**"],
-          "groups": [
-            ["side-effect", "side-effect-style"],
-            ["builtin-type", "external-type", "builtin", "external"],
-            ["internal-type", "internal"],
-            ["parent-type", "sibling-type", "index-type", "parent", "sibling", "index"],
-            "object",
-            "unknown",
-          ],
-        }],
+        "perfectionist/sort-imports": [
+          "error",
+          {
+            "type": "line-length",
+            "newlines-between": "always",
+            "internal-pattern": ["@/**", "~/**"],
+            "groups": [
+              ["side-effect", "side-effect-style"],
+              ["builtin-type", "external-type", "builtin", "external"],
+              ["internal-type", "internal"],
+              [
+                "parent-type",
+                "sibling-type",
+                "index-type",
+                "parent",
+                "sibling",
+                "index",
+              ],
+              "object",
+              "unknown",
+            ],
+          },
+        ],
         "perfectionist/sort-named-exports": "error",
         "perfectionist/sort-named-imports": "error",
       },
@@ -113,5 +137,19 @@ export function totominc() {
     ...compat.config({
       extends: ["plugin:@next/next/recommended", "plugin:jsx-a11y/recommended"],
     }),
+    ...userConfigs,
   );
 }
+
+export {
+  GLOB_ALL_SRC,
+  GLOB_HTML,
+  GLOB_JS,
+  GLOB_JSON,
+  GLOB_JSX,
+  GLOB_SRC,
+  GLOB_STYLE,
+  GLOB_TESTS,
+  GLOB_TS,
+  GLOB_TSX,
+} from "@antfu/eslint-config";
